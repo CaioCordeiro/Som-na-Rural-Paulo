2a9143bf641cafe5807aa786a4b80c5c69a74383
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-06-14 10:13:33 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -4,12 +4,24 @@ import sys
 import time
 import datetime
 import serial
-
+import facebook
+from PIL import Image
+import numpy
+counter = 0
 WEBCAM_DEVICE = 0
 STORAGE_DIR = 'storage'
 
-ArduinoUnoSerial = serial.Serial('/dev/cu.usbmodem1421', 9600)
+ArduinoUnoSerial = serial.Serial('COM4', 9600)
 
+graph = facebook.GraphAPI(access_token="EAACEdEose0cBAKZAlZC9lIKn9P2ny3KIZAHZCGSsJgRTiHfyBChKlJ8ZC5P4vWnnYxpp7j7kNsOjM9UHJkNzVrSENWWBuQN9rAjXn3HqbY6GCvAhoIfshVVwiJa5A8o5RlyaEi89KlR2O6FPQwQOeSZAJ1DLg5SoOoJMulLzOTRixcgUesm3mfM76nB019hv3Y0fgRB2P7agZDZD")
+def imgedit(foto): 
+
+    background = Image.open(foto)
+    foreground = Image.open("fg.png")
+    background.paste(foreground, (0, 0), foreground)
+    background.save('editado'+str(counter)+'.jpg')
+   
+    graph.put_photo(image=open('editado'+str(counter)+'.jpg', 'rb'), message='Look at this cool photo!')
 
 def putText(img, text, location, positive=True):
 
@@ -65,6 +77,7 @@ if __name__ == '__main__':
                 cv2.waitKey(3)
 
             if dado_recebido == b'1':  # serial read 1: print
+                conter = counter+1
                 img_ui = img.copy()
                 putText(img_ui, "SALVANDO(5)", 'centre', True)
                 cv2.imshow(screen, img_ui)
@@ -74,7 +87,13 @@ if __name__ == '__main__':
                 filename = os.path.join(
                     os.getcwd(), STORAGE_DIR, datestr + ".jpg")
                 cv2.imwrite(filename, img)
+               
+                imgedit(filename)
+                counter = counter + 1
 
+
+                
+              
                 for i in range(4, 0, -1):
                     time.sleep(1)
                     img_ui = img.copy()
