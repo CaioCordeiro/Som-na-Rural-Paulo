c557dc42470da493e1816f0933b9d92db7fff27c
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-06-14 15:23:49 -0300

    Change text font

--- Python/main.py
+++ Python/main.py
@@ -14,34 +14,46 @@ STORAGE_DIR = 'storage'
 ArduinoUnoSerial = serial.Serial('COM4', 9600)
 
 
-graph = facebook.GraphAPI(access_token="EAACEdEose0cBAKZAlZC9lIKn9P2ny3KIZAHZCGSsJgRTiHfyBChKlJ8ZC5P4vWnnYxpp7j7kNsOjM9UHJkNzVrSENWWBuQN9rAjXn3HqbY6GCvAhoIfshVVwiJa5A8o5RlyaEi89KlR2O6FPQwQOeSZAJ1DLg5SoOoJMulLzOTRixcgUesm3mfM76nB019hv3Y0fgRB2P7agZDZD")
+graph = facebook.GraphAPI(
+    access_token="EAACEdEose0cBAKZAlZC9lIKn9P2ny3KIZAHZCGSsJgRTiHfyBChKlJ8ZC5P4vWnnYxpp7j7kNsOjM9UHJkNzVrSENWWBuQN9rAjXn3HqbY6GCvAhoIfshVVwiJa5A8o5RlyaEi89KlR2O6FPQwQOeSZAJ1DLg5SoOoJMulLzOTRixcgUesm3mfM76nB019hv3Y0fgRB2P7agZDZD")
 
-def imgedit(foto): 
+
+def imgedit(foto):
 
     background = Image.open(foto)
-    foreground = Image.open("fg.png")
+    foreground = Image.open("filtro.png")
     background.paste(foreground, (0, 0), foreground)
     background.save('editado'+str(counter)+'.jpg')
-   
-    graph.put_photo(image=open('editado'+str(counter)+'.jpg', 'rb'), message='Look at this cool photo!')
+
+    graph.put_photo(image=open('editado'+str(counter)+'.jpg',
+                               'rb'), message='Look at this cool photo!')
+
+
+def screenEdit(screen):
+
+    background = Image.open(screen)
+    foreground = Image.open("filtro.png")
+    background.paste(foreground, (0, 0), foreground)
+    return background
+
 
 def putText(img, text, location, positive=True):
 
-    font = cv2.FONT_HERSHEY_DUPLEX
-    fsize = 2
-    colour = (0, 255, 0) if positive else (255, 0, 0)
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    fsize = 3
+    colour = (0, 255, 0) if positive else (0, 0, 255)
     if location == 'left_button':
         cv2.putText(img, text, (0, img.shape[0] - 10),
                     font, fsize, colour)
     elif location == 'right_button':
         cv2.putText(
             img, text,
-            (int(img.shape[1] - 40*len(text)), int(img.shape[0] - 10)),
+            (int(img.shape[1] - 60*len(text)), int(img.shape[0] - 10)),
             font, fsize, colour)
     elif location == 'centre':
         cv2.putText(
             img, text,
-            (int(img.shape[1] / 2 - 20*len(text)), int(img.shape[0] / 2)),
+            (int(img.shape[1] / 2 - 30*len(text)), int(img.shape[0] / 2)),
             font, fsize, colour)
 
 
@@ -89,11 +101,10 @@ if __name__ == '__main__':
                 filename = os.path.join(
                     os.getcwd(), STORAGE_DIR, datestr + ".jpg")
                 cv2.imwrite(filename, img)
-               
+
                 imgedit(filename)
                 counter = counter + 1
-                
-              
+
                 for i in range(4, 0, -1):
                     time.sleep(1)
                     img_ui = img.copy()
@@ -106,4 +117,3 @@ if __name__ == '__main__':
                 continue  # retake
 
     cv2.waitKey(1)
-
